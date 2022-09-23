3017797662af1b64d6626912841128574c1b7e15
Mario Gomez Morales <mario_bersain99@hotmail.com>
2022-09-23 18:12:16 -0500

    Códido terminado sobre áreas de Cuadrado, Triángulo y Círculo

---
+++ CalcularArea/CalcularArea/AreaWebService.asmx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CalcularArea
+{
+    /// <summary>
+    /// Descripción breve de AreaWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class AreaWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+        [WebMethod]
+        public decimal Cuadrado(decimal Base, decimal Altura)
+        {
+            CalcularArea c = new CalcularArea();
+            {
+                c.num1 = Base;
+                c.num2 = Altura;
+            }
+            return c.Cuadrado();
+        }
+        [WebMethod]
+        public decimal Triangulo(decimal Base, decimal Altura)
+        {
+            CalcularArea c = new CalcularArea();
+            return c.Triangulo(Base, Altura);
+        }
+        [WebMethod]
+        public decimal Circulo(double Radio)
+        {
+            CalcularArea c = new CalcularArea();
+            return c.Circulo(Radio);
+        }
+    }
+}
---
+++ CalcularArea/CalcularArea/CalcularArea.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalcularArea
+{
+    public class CalcularArea
+    {
+        public decimal num1 { get; set; }
+        public decimal num2 { get; set; }
+        public decimal Cuadrado()
+        {
+            return num1 * num2;
+        }
+        public decimal Triangulo(decimal n1, decimal n2)
+        {
+            num1 = n1;
+            num2 = n2;
+            return num1 * num2 / 2;
+        }
+        public decimal Circulo(double n1)
+        {
+            num1 = (decimal)(double)n1;
+            double var;
+            double pi = 3.141592;
+            var = pi * (n1 * n1);
+            return (decimal)var;
+        }
+    }
+}
